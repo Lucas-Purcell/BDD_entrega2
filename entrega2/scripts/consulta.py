4298eb44963b421d3247cde6fae41126f4d9e0a7
Victoria Vásquez Díaz <victoriavasquezdiaz@uc.cl>
2024-05-22 14:39:52 -0400

    revision queries 10

---
+++ entrega2/scripts/consulta.py
@@ -0,0 +1,48 @@
+#REVISAR LOS OUTPUTS QUE SE MUESTRAN, VER SI ARGEGAMOS MÁS O SI SACAMOS ALGUNAS. 
+
+
+#Muestre todos los restaurantes que ofrezcan un plato específico (string) ingresado y que esté disponible
+def consulta1(): 
+    #query devuelve id, nombre y sucursal de los restaurantes que ofrecen el plato ingresado
+    query = """SELECT Restaurant.id, Restaurant.nombre, Restaurant.sucursal 
+        FROM Plato
+        JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
+        JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
+        WHERE Plato.nombre = %(nombre)s 
+        AND Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
+    return query
+
+
+#Dado un estilo de plato ingresado (string) por el usuario, muestre todas las platos que pertenezcan a ese estilo y sus restricciones
+def consulta5():
+    #query devuelve id, nombre, sucursal de restauramt, y nombre y restricción de los platos 
+    query = """SELECT Restaurant. id, Restaurant.nombre, Restaurant.sucursal, Plato.nombre, Plato.restriccion
+        FROM Plato
+        JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
+        JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
+        WHERE Plato.estilo = %(estilo)s 
+        AND Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
+    return query
+
+#Dado una un alergeno ingresado por el usuario (string), muestre todos los platos que lo contengan en sus ingredientes (Ejemplo: maní)
+def consulta10():
+    query = """SELECT Restaurant.id, Restaurant.nombre, Restaurant.sucursal, Plato.nombre 
+        FROM Plato
+        JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
+        JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
+        WHERE Plato.ingredientes LIKE %(alergeno)s 
+        AND Plato.disponibilidad = TRUE"""
+    return query
+
+#Muestre todos los platos y los restaurantes que los ofrecen
+def consulta8():
+    #query devuelve nombre del plato, id, nombre y sucursal del restaurante
+    query = """SELECT Plato.nombre, Restaurant.id, Restaurant.nombre, Restaurant.sucursal 
+        FROM Plato
+        JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
+        JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
+        WHERE Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
+    return query
---
+++ entrega2/scripts/manejo_consultas.py
@@ -0,0 +1,150 @@
+import psycopg2
+from psycopg2 import sql
+from psycopg2 import Error
+from consultas import (consulta1, consulta5, consulta8, consulta10)
+
+#para conectarse a la base
+DB_HOST = 'pavlov.ing.puc.cl'
+DB_PORT = '5432'
+DB_USER = 'grupo8'
+DB_PASSWORD = 'bulla8'
+DB_NAME = 'grupo8e2'
+
+def consultas(numero_consulta, **kwargs):
+    conn = None
+    cursor = None
+    
+    try:
+        # Conexión con la base de datos
+        conn = psycopg2.connect(
+            user = DB_USER,
+            password = DB_PASSWORD,
+            host = DB_HOST,
+            port = DB_PORT,
+            dbname = DB_NAME
+        )
+        
+        # Crear un cursor para ejecutar consultas
+        cursor = conn.cursor()
+
+        if numero_consulta == 1:
+            try:
+                query = consulta1()
+                nombre_plato = kwargs.get('nombre_plato')
+                cursor.execute(query, {'nombre': nombre_plato})
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+            except (Exception, Error) as error:
+                print("Error al ejecutar query:", error)
+
+        # if numero_consulta == 2:
+        #     try:
+        #         query, query1 = consulta2()
+        #         email = kwargs.get('email')
+        #         fecha_inicio = kwargs.get('fecha_inicio')
+        #         fecha_fin = kwargs.get('fecha_fin')
+        #         cursor.execute(query, {'email': email})
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #         cursor.execute(query1, {'fecha_inicio': fecha_inicio, 'fecha_fin': fecha_fin})
+        #         rows = cursor.fetchall()
+        #         print(rows)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        # if numero_consulta == 3:
+        #     try:
+        #         query = consulta3()
+        #         cursor.execute(query)
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        # if numero_consulta == 4:
+        #     try:
+        #         query = consulta4()
+        #         estilo = kwargs.get('estilo')
+        #         cursor.execute(query, {'estilo': estilo})
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        if numero_consulta == 5:
+            try:
+                query = consulta5()
+                estilo = kwargs.get('estilo')
+                cursor.execute(query, {'estilo': estilo})
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+            except (Exception, Error) as error:
+                print("Error al ejecutar query:", error)
+
+        # if numero_consulta == 6:
+        #     try:
+        #         query = consulta6()
+        #         email = kwargs.get('email')
+        #         cursor.execute(query, {'email': email})
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        # if numero_consulta == 7:
+        #     try:
+        #         query = consulta7()
+        #         cursor.execute(query)
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        if numero_consulta == 8:
+            try:
+                query = consulta8()
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+            except (Exception, Error) as error:
+                print("Error al ejecutar query:", error)
+        
+        # if numero_consulta == 9:
+        #     try:
+        #         tipo = kwargs.get('tipo')
+        #         query = consulta9(tipo)
+        #         numero = kwargs.get('numero')
+        #         cursor.execute(query, {'numero': numero})
+        #         rows = cursor.fetchall()
+        #         for row in rows:
+        #             print(row)
+        #     except (Exception, Error) as error:
+        #         print("Error al ejecutar query:", error)
+
+        if numero_consulta == 10:
+            try:
+                query = consulta10()
+                alergeno = kwargs.get('alergeno')
+                cursor.execute(query, {'alergeno': '%{}%'.format(alergeno)}) #revisar si esto funciona
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+            except (Exception, Error) as error:
+                print("Error al ejecutar query:", error)
+
+    except (Exception, Error) as error:
+        print("Error al conectarse al servidor:", error)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
