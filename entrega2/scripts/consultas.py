941aedb796be1dbdf9944dff135ea8560689b133
Victoria Vásquez Díaz <victoriavasquezdiaz@uc.cl>
2024-05-24 11:23:47 -0400

    consultas

--- entrega2/scripts/consultas.py
+++ entrega2/scripts/consultas.py
@@ -1,3 +1,6 @@
+#REVISAR LOS OUTPUTS QUE SE MUESTRAN, VER SI ARGEGAMOS MÁS O SI SACAMOS ALGUNAS. 
+
+
 #Muestre todos los restaurantes que ofrezcan un plato específico (string) ingresado y que esté disponible
 def consulta1(): 
     #query devuelve id, nombre y sucursal de los restaurantes que ofrecen el plato ingresado
@@ -6,7 +9,23 @@ def consulta1():
         JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
         JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
         WHERE Plato.nombre = %(nombre)s 
-        AND Plato.disponibilidad = TRUE """
+        AND Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
+    return query
+
+def consulta4():
+    #query devuelve nombre del plato, id, nombre y sucursal del restaurante, nombre de empresa
+    query = """SELECT Plato.nombre, Res_em.id_restaurant, Res_em.nombre_restaurant, Res_em.sucursal, Res_em.nombre_empresa
+        FROM Plato
+        JOIN PlatoRestaurant ON Plato.id = PlatoRestaurant.plato_id
+        JOIN (SELECT Restaurant.id as id_restaurant, Restaurant.nombre as nombre_restaurant, Restaurant.sucursal, Empresa.nombre as nombre_empresa
+                    FROM Restaurant
+                    JOIN ConvenioEmpresaRestaurant ON Restaurant.id = ConvenioEmpresaRestaurant.restaurant_id
+                    JOIN Empresa ON Empresa.id = ConvenioEmpresaRestaurant.empresa_id
+                    WHERE Restaurant.vigente = TRUE) AS Res_em ON PlatoRestaurant.restaurant_id = Res_em.id_restaurant
+        WHERE Plato.estilo = %(estilo)s 
+        AND Plato.disponibilidad = TRUE"""
+    
     return query
 
 
@@ -18,7 +37,8 @@ def consulta5():
         JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
         JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
         WHERE Plato.estilo = %(estilo)s 
-        AND Plato.disponibilidad = TRUE"""
+        AND Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
     return query
 
 #Muestre todos los platos y los restaurantes que los ofrecen
@@ -28,7 +48,18 @@ def consulta8():
         FROM Plato
         JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
         JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
-        WHERE Plato.disponibilidad = TRUE"""
+        WHERE Plato.disponibilidad = TRUE
+        AND Restaurant.vigente = TRUE"""
     return query
 
 
+#Dado una un alergeno ingresado por el usuario (string), muestre todos los platos que lo contengan en sus ingredientes (Ejemplo: maní)
+def consulta10():
+    #query devuelve id, nombre y sucursal de restauramt, y nombre de los platos
+    query = """SELECT Restaurant.id, Restaurant.nombre, Restaurant.sucursal, Plato.nombre 
+        FROM Plato
+        JOIN Platorestaurant ON Plato.id = Platorestaurant.plato_id 
+        JOIN Restaurant ON Platorestaurant.restaurant_id = Restaurant.id
+        WHERE Plato.ingredientes LIKE %(alergeno)s 
+        AND Plato.disponibilidad = TRUE"""
+    return query
