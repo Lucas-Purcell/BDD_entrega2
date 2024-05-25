a09f244612c7218f26a91abff8238998818c2011
Victoria Vásquez Díaz <victoriavasquezdiaz@uc.cl>
2024-05-25 14:04:44 -0400

    pagina

--- entrega2/Proyecto/EasyFood/urls.py
+++ entrega2/Proyecto/EasyFood/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Example URL pattern
-    path('consultas/', views.consultas, name='consultas'),
+    path('', views.home, name='home'),  
+    path('consultas_predeterminadas/', views.consultas_predeterminadas, name='consultas_predeterminadas'),
     # Add more app-specific URL patterns for views
 ]
