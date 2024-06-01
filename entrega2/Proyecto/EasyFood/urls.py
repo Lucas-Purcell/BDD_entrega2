195a9eaed1c3ce4a3708b971fd1ba35ff4dc6354
Juan Andrés <j_munoz@uc.cl>
2024-06-01 19:32:18 -0400

    Consultas Inestructuradas
    Implementación completa

--- entrega2/Proyecto/EasyFood/urls.py
+++ entrega2/Proyecto/EasyFood/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  
     path('consultas_predeterminadas/', views.consultas_predeterminadas, name='consultas_predeterminadas'),
+    path('consultas_inestructuradas/', views.consultas_inestructuradas, name='consultas_inestructuradas'),
     # Add more app-specific URL patterns for views
 ]
