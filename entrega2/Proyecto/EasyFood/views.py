d36b722e5d527c15f36dfba751b94324642a9cb8
Victoria Vásquez Díaz <victoriavasquezdiaz@uc.cl>
2024-06-02 16:51:19 -0400

    'consultas final'

--- entrega2/Proyecto/EasyFood/views.py
+++ entrega2/Proyecto/EasyFood/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
-from .consultas import consulta1, consulta4, consulta5, consulta6, consulta8, consulta9, consulta10
+from .consultas import consulta1, consulta2, consulta3, consulta4, consulta5, consulta6, consulta7, consulta8, consulta9, consulta10
 from django import forms
+
 
 def home(request):
     return render(request, 'home.html')
@@ -23,10 +24,19 @@ def consultas_predeterminadas(request):
 
         result = None
         column_names = []
+        result1 = None
+        column_names1 = []
 
         if consulta == 1:
             query = consulta1()
             result, column_names = ejecutar_consulta(query, {'nombre': params.get('nombre')})
+        elif consulta == 2:
+            query, query1 = consulta2()
+            result, column_names = ejecutar_consulta(query, {'email': params.get('email')})
+            result1, column_names1 = ejecutar_consulta(query1, {'email': params.get('email')})
+        elif consulta == 3:
+            query = consulta3()
+            result, column_names = ejecutar_consulta(query)
         elif consulta == 4:
             query = consulta4()
             result, column_names = ejecutar_consulta(query, {'estilo': params.get('estilo')})
@@ -36,6 +46,9 @@ def consultas_predeterminadas(request):
         elif consulta == 6:
             query = consulta6()
             result, column_names = ejecutar_consulta(query, {'email': params.get('email')})
+        elif consulta == 7:
+            query = consulta7()
+            result, column_names = ejecutar_consulta(query)
         elif consulta == 8:
             query = consulta8()
             result, column_names = ejecutar_consulta(query)
@@ -44,11 +57,15 @@ def consultas_predeterminadas(request):
             result, column_names = ejecutar_consulta(query, {'numero': params.get('numero')})
         elif consulta == 10:
             query = consulta10()
-            result, column_names = ejecutar_consulta(query, {'alergeno': params.get('alergeno')})
-            print(result)
+            result, column_names = ejecutar_consulta(query, {'alergeno': '%{}%'.format(params.get('alergeno'))})
 
-        return render(request, 'consultas_predeterminadas.html', {'results': result, 'column_names': column_names})
-
+        return render(request, 'consultas_predeterminadas.html', {
+            'results': result,
+            'column_names': column_names,
+            'results1': result1,
+            'column_names1': column_names1,
+        })
+        
     return render(request, 'consultas_predeterminadas.html')
 
 # Consulta Inestructuradas
